97d97b06f4b8d062cefe43c1d9697f7c562a39b9
ouharri <ouharrioutman@gmail.com>
2023-12-10 06:36:42 +0100

    DTO class representing the response for authentication. Contains access and refresh tokens.

---
+++ src/main/java/com/ouharri/aftas/model/dto/auth/AuthenticationResponse.java
@@ -0,0 +1,37 @@
+package com.ouharri.aftas.model.dto.auth;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ * DTO class representing the response for authentication.
+ * Contains access and refresh tokens.
+ *
+ * @author Ouharri Outman
+ * @version 1.0
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+  /**
+   * Access token for authentication.
+   */
+  @NotBlank(message = "Access token is required")
+  @JsonProperty("access_token")
+  private String accessToken;
+
+  /**
+   * Refresh token for token refreshing.
+   */
+  @NotBlank(message = "Refresh token is required")
+  @JsonProperty("refresh_token")
+  private String refreshToken;
+}
