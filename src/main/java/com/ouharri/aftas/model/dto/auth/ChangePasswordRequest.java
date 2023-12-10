6a6cea6601e688f23febf9436181f66514782ae5
ouharri <ouharrioutman@gmail.com>
2023-12-10 19:04:00 +0100

    ChangePassword Request Dto

--- src/main/java/com/ouharri/aftas/model/dto/auth/ChangePasswordRequest.java
+++ src/main/java/com/ouharri/aftas/model/dto/auth/ChangePasswordRequest.java
@@ -1,9 +1,5 @@
 package com.ouharri.aftas.model.dto.auth;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -14,27 +10,11 @@ import jakarta.validation.constraints.Size;
  * @author Ouharri Outman
  * @version 1.0
  */
-@Getter
-@Setter
-@Builder
-public class ChangePasswordRequest {
-    /**
-     * The user's current password.
-     */
-    @NotBlank(message = "Current password is required")
-    private String currentPassword;
-
-    /**
-     * The new password to be set.
-     */
-    @NotBlank(message = "New password is required")
-    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
-    private String newPassword;
-
-    /**
-     * Confirmation of the new password.
-     */
-    @NotBlank(message = "Password confirmation is required")
-    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
-    private String confirmationPassword;
+public record ChangePasswordRequest(
+        @NotBlank(message = "Current password is required") String currentPassword,
+        @NotBlank(message = "New password is required")
+        @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters") String newPassword,
+        @NotBlank(message = "Password confirmation is required")
+        @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters") String confirmationPassword
+) {
 }
