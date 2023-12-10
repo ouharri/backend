0e4be932e340c00aab4427f818d16d153a3a0d98
ouharri <ouharrioutman@gmail.com>
2023-12-10 06:48:01 +0100

    Controller class for handling user-related endpoints.

--- src/main/java/com/ouharri/aftas/controllers/UserController.java
+++ src/main/java/com/ouharri/aftas/controllers/UserController.java
@@ -1,6 +1,6 @@
 package com.ouharri.aftas.controllers;
 
-import com.ouharri.aftas.model.dto.ChangePasswordRequest;
+import com.ouharri.aftas.model.dto.auth.ChangePasswordRequest;
 import com.ouharri.aftas.model.entities.User;
 import com.ouharri.aftas.services.impl.UserService;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +11,12 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 import java.util.List;
 
+/**
+ * Controller class for handling user-related endpoints.
+ *
+ * @author Ouharri Outman
+ * @version 1.0
+ */
 @RestController
 @RequestMapping("/api/v2/users")
 @RequiredArgsConstructor
@@ -18,16 +24,28 @@ public class UserController {
 
     private final UserService service;
 
+    /**
+     * Retrieves a list of all users.
+     *
+     * @return ResponseEntity containing the list of users and HTTP status OK.
+     */
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = service.getAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
+    /**
+     * Changes the password of the currently logged-in user.
+     *
+     * @param request        The change password request.
+     * @param connectedUser  The principal representing the currently connected user.
+     * @return ResponseEntity with HTTP status OK.
+     */
     @PatchMapping
     public ResponseEntity<?> changePassword(
-          @RequestBody ChangePasswordRequest request,
-          Principal connectedUser
+            @RequestBody ChangePasswordRequest request,
+            Principal connectedUser
     ) {
         service.changePassword(request, connectedUser);
         return ResponseEntity.ok().build();
