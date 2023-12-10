d3c43d7539f31a38a8a0ceca47b0b0233121b4d1
ouharri <ouharrioutman@gmail.com>
2023-12-10 19:08:32 +0100

    Fish Repository

---
+++ src/main/java/com/ouharri/aftas/repositories/FishRepository.java
@@ -0,0 +1,9 @@
+package com.ouharri.aftas.repositories;
+
+import com.ouharri.aftas.model.entities.Fish;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface FishRepository extends JpaRepository<Fish, UUID> {
+}
