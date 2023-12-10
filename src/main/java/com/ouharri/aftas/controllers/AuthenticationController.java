9c307867bc9920d0d46b5981b1ddd3f7d1921549
ouharri <ouharrioutman@gmail.com>
2023-12-10 06:28:24 +0100

    Controller class for handling authentication-related requests.This class provides endpoints for user registration, authentication, token validation, and token refreshing.

--- src/main/java/com/ouharri/aftas/controllers/AuthenticationController.java
+++ src/main/java/com/ouharri/aftas/controllers/AuthenticationController.java
@@ -1,9 +1,9 @@
 package com.ouharri.aftas.controllers;
 
-import com.ouharri.aftas.model.dto.AuthenticationRequest;
-import com.ouharri.aftas.model.dto.AuthenticationResponse;
-import com.ouharri.aftas.model.dto.RegisterRequest;
 import com.ouharri.aftas.exceptions.ResourceNotCreatedException;
+import com.ouharri.aftas.model.dto.auth.AuthenticationRequest;
+import com.ouharri.aftas.model.dto.auth.AuthenticationResponse;
+import com.ouharri.aftas.model.dto.auth.RegisterRequest;
 import com.ouharri.aftas.services.spec.IAuthenticationService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,13 +13,17 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
+/**
+ * Controller class for handling authentication-related requests.
+ * This class provides endpoints for user registration, authentication,
+ * token validation, and token refreshing.
+ *
+ * @author <a href="mailto:ouharri.outman@gmail.com">Ouharri Outman</a>
+ */
 @Validated
 @RestController
 @RequestMapping("/api/v2/auth")
@@ -27,6 +31,14 @@ import java.io.IOException;
 public class AuthenticationController {
     private final IAuthenticationService service;
 
+    /**
+     * Endpoint for user registration.
+     *
+     * @param request        The registration request containing user details.
+     * @param bindingResult  BindingResult for validation errors.
+     * @return               ResponseEntity with the registered user's authentication response.
+     * @throws ResourceNotCreatedException If there are validation errors.
+     */
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @Valid @RequestBody final RegisterRequest request,
@@ -38,13 +50,31 @@ public class AuthenticationController {
         return ResponseEntity.ok(service.register(request));
     }
 
+    /**
+     * Endpoint for user authentication.
+     *
+     * @param request        The authentication request containing user credentials.
+     * @param bindingResult  BindingResult for validation errors.
+     * @return               ResponseEntity with the authenticated user's response.
+     * @throws ResourceNotCreatedException If there are validation errors.
+     */
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(
-            @Valid @RequestBody final AuthenticationRequest request
+            @Valid @RequestBody final AuthenticationRequest request,
+            BindingResult bindingResult
     ) {
+        if (bindingResult.hasErrors())
+            throw new ResourceNotCreatedException(bindingResult);
+
         return ResponseEntity.ok(service.authenticate(request));
     }
 
+    /**
+     * Endpoint for checking the validity of an authentication token.
+     *
+     * @param request The HTTPServletRequest containing the token in the Authorization header.
+     * @return        ResponseEntity with a boolean indicating token validity.
+     */
     @PostMapping("/check-token")
     public ResponseEntity<Boolean> checkToken(
             HttpServletRequest request
@@ -53,6 +83,13 @@ public class AuthenticationController {
         return ResponseEntity.ok(service.checkToken(authHeader));
     }
 
+    /**
+     * Endpoint for refreshing an authentication token.
+     *
+     * @param request  The HTTPServletRequest containing the current token.
+     * @param response The HTTPServletResponse for sending the new token.
+     * @throws IOException If there is an issue handling the token refresh.
+     */
     @PostMapping("/refresh-token")
     public void refreshToken(
             HttpServletRequest request,
@@ -60,5 +97,4 @@ public class AuthenticationController {
     ) throws IOException {
         service.refreshToken(request, response);
     }
-
 }
