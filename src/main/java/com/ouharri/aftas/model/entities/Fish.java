58d1ec456b5a73b41df8154f59a6751c78c91420
ouharri <ouharrioutman@gmail.com>
2023-12-10 19:05:54 +0100

    Fish Entity

---
+++ src/main/java/com/ouharri/aftas/model/entities/Fish.java
@@ -0,0 +1,33 @@
+package com.ouharri.aftas.model.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "fish")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Fish extends AbstractEntity {
+    @NotBlank(message = "The name cannot be blank.")
+    private String name;
+
+    @NotNull(message = "The average weight cannot be null.")
+    @DecimalMin(value = "0.0", inclusive = false, message = "The average weight must be greater than 0.")
+    private Double averageWeight;
+
+    @ManyToOne
+    @JoinColumn(name = "level_id", referencedColumnName = "id")
+    private Level level;
+
+    @OneToMany(mappedBy = "fish", cascade = CascadeType.ALL)
+    private List<Hunting> huntings = new ArrayList<>();
+}
