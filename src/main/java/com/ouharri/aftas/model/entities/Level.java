5582fa8d23f709d7cf814a3a694ecd03b25560d6
ouharri <ouharrioutman@gmail.com>
2023-12-10 18:58:35 +0100

    The Level Entity

---
+++ src/main/java/com/ouharri/aftas/model/entities/Level.java
@@ -0,0 +1,33 @@
+package com.ouharri.aftas.model.entities;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "level")
+public class Level extends AbstractEntity {
+    @NotBlank(message = "Name cannot be blank.")
+    private String name;
+
+    @NotBlank(message = "Description cannot be blank.")
+    private String description;
+
+    @NotNull(message = "Points cannot be null.")
+    private Integer points;
+
+    @OneToMany(mappedBy = "level", cascade = CascadeType.ALL)
+    private List<Fish> fishes = new ArrayList<>();
+}
