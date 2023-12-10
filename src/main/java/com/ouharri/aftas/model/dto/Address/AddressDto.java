3a8abde9f1bf30d426bc6cdee4c31a70096a0900
ouharri <ouharrioutman@gmail.com>
2023-12-10 19:01:30 +0100

    Address Request Dto

---
+++ src/main/java/com/ouharri/aftas/model/dto/Address/AddressDto.java
@@ -0,0 +1,20 @@
+package com.ouharri.aftas.model.dto.Address;
+
+import com.ouharri.aftas.model.entities.Address;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link Address}
+ */
+public record AddressDto(@Size(message = "Region name is too long", max = 30) String region,
+                         @Size(message = "District name is too long", max = 30) String district,
+                         @Size(message = "City name is too long", max = 30) String city,
+                         @Size(message = "Street name is too long", max = 30) String street,
+                         @Min(message = "Building number must be greater than 0", value = 1) @Max(message = "Building number is too big", value = 32767) Integer building,
+                         @Min(message = "Apartment number must be greater than 0", value = 1) @Max(message = "Apartment number is too big", value = 32767) Integer apartment,
+                         @Min(message = "Invalid postal code", value = 1001) @Max(message = "Invalid postal code", value = 99999) Integer postalCode) implements Serializable {
+}
