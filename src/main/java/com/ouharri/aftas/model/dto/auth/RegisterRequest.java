0b2d445d425f98e7a465411120482e4b0841d080
ouharri <ouharrioutman@gmail.com>
2023-12-10 19:10:05 +0100

    Register Request

--- src/main/java/com/ouharri/aftas/model/dto/auth/RegisterRequest.java
+++ src/main/java/com/ouharri/aftas/model/dto/auth/RegisterRequest.java
@@ -5,11 +5,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 
 /**
  * DTO class representing a user registration request.
@@ -18,29 +13,21 @@ import lombok.NoArgsConstructor;
  * @author Ouharri Outman
  * @version 1.0
  */
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class RegisterRequest {
-  @NotNull(message = "First name must be present")
-  @Size(min = 1, message = "First name cannot be empty")
-  @Size(max = 30, message = "First name is too long")
-  private String firstname;
+public record RegisterRequest(
+        @NotNull(message = "First name must be present")
+        @Size(min = 1, message = "First name cannot be empty")
+        @Size(max = 30, message = "First name is too long") String firstname,
 
-  @Size(max = 30, message = "Last name is too long")
-  private String lastname;
+        @Size(max = 30, message = "Last name is too long") String lastname,
 
-  @Email(message = "Email was not provided")
-  @Size(max = 80, message = "Email is too long")
-  @Column(unique = true)
-  private String email;
+        @Email(message = "Email was not provided")
+        @Size(max = 80, message = "Email is too long")
+        @Column(unique = true, table = "_user") String email,
 
-  @NotEmpty(message = "Password cannot be empty")
-  @Size(min = 8, message = "Password is too short")
-  private String password;
+        @NotEmpty(message = "Password cannot be empty")
+        @Size(min = 8, message = "Password is too short") String password,
 
-  @NotEmpty(message = "Password confirmation cannot be empty")
-  @Size(min = 8, message = "Password confirmation is too short")
-  private String confirmationPassword;
+        @NotEmpty(message = "Password confirmation cannot be empty")
+        @Size(min = 8, message = "Password confirmation is too short") String confirmationPassword
+) {
 }
