289b3230533113c29a9807b8e92bea1da8e7b014
ouharri <ouharrioutman@gmail.com>
2023-12-11 18:39:56 +0100

    Marker interface for request data transfer objects (DTOs). Implement this interface in all your request DTO classes. This interface serves as a common type for all request DTOs.

---
+++ src/main/java/com/ouharri/aftas/model/dto/requests/Request.java
@@ -0,0 +1,13 @@
+package com.ouharri.aftas.model.dto.requests;
+
+import java.io.Serializable;
+
+/**
+ * Marker interface for request data transfer objects (DTOs).
+ * Implement this interface in all your request DTO classes.
+ * This interface serves as a common type for all request DTOs.
+ */
+public interface Request extends Serializable {
+    // This interface is intentionally left blank.
+    // It serves as a marker interface for request DTOs.
+}
