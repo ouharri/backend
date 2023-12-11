d713771435a000e6759daeada54c8e725505b95a
ouharri <ouharrioutman@gmail.com>
2023-12-11 18:41:12 +0100

    Marker interface for response data transfer objects (DTOs). Implement this interface in all your response DTO classes. This interface serves as a common type for all response DTOs.

---
+++ src/main/java/com/ouharri/aftas/model/dto/responces/Response.java
@@ -0,0 +1,13 @@
+package com.ouharri.aftas.model.dto.responces;
+
+import java.io.Serializable;
+
+/**
+ * Marker interface for response data transfer objects (DTOs).
+ * Implement this interface in all your response DTO classes.
+ * This interface serves as a common type for all response DTOs.
+ */
+public interface Response extends Serializable {
+    // This interface is intentionally left blank.
+    // It serves as a marker interface for response DTOs.
+}
