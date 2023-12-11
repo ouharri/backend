e779771c54d424db0bcc3fee8a911f44df872720
ouharri <ouharrioutman@gmail.com>
2023-12-11 18:37:49 +0100

    Generic service interface with common CRUD operations.

---
+++ src/main/java/com/ouharri/aftas/services/spec/_service.java
@@ -0,0 +1,58 @@
+package com.ouharri.aftas.services.spec;
+
+import com.ouharri.aftas.model.dto.requests.Request;
+import com.ouharri.aftas.model.dto.responces.Response;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Generic service interface with common CRUD operations.
+ *
+ * @param <Req> The request DTO type.
+ * @param <Res> The response DTO type.
+ */
+public interface _service<Req extends Request, Res extends Response> {
+
+    /**
+     * Retrieves all entities in a paginated form.
+     *
+     * @param pageable Pagination information.
+     * @return Page of response DTOs.
+     */
+    Page<Res> getAll(Pageable pageable);
+
+    /**
+     * Creates a new entity based on the provided request DTO.
+     *
+     * @param request DTO containing data for entity creation.
+     * @return Optional containing the response DTO of the created entity.
+     */
+    Optional<Res> create(Req request);
+
+    /**
+     * Updates an existing entity based on the provided response DTO.
+     *
+     * @param response DTO containing updated data.
+     * @return Optional containing the response DTO of the updated entity.
+     */
+    Optional<Res> update(Res response);
+
+    /**
+     * Retrieves an entity by its unique identifier.
+     *
+     * @param id Unique identifier of the entity.
+     * @return Optional containing the response DTO of the found entity.
+     */
+    Optional<Res> getById(UUID id);
+
+    /**
+     * Deletes an entity based on the provided response DTO.
+     *
+     * @param response DTO containing data for entity deletion.
+     * @return Boolean indicating the success of the deletion operation.
+     */
+    Boolean delete(Res response);
+}
