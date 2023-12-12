fe3c59c1f5f6bf6f02ff154fd7c017646c8e8266
ouharri <ouharrioutman@gmail.com>
2023-12-12 17:13:59 +0100

    Represents a user entity in the system

--- src/main/java/com/ouharri/aftas/model/entities/User.java
+++ src/main/java/com/ouharri/aftas/model/entities/User.java
@@ -17,84 +17,160 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-@Entity
+/**
+ * Represents a user entity in the system.
+ *
+ * @author ouharri
+ * @version 2.0
+ */
 @Getter
 @Setter
 @Builder
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "_user")
 public class User extends AbstractEntity implements UserDetails {
 
+    /**
+     * The user's password.
+     */
     private String password;
 
+    /**
+     * The path to the user's image.
+     */
     private String image;
 
+    /**
+     * The user's phone number.
+     */
     @Pattern(regexp = "0\\d{9}", message = "Phone number must match the format '0XXXXXXXXX'")
     @Column(unique = true)
     private String phoneNumber;
 
+    /**
+     * The user's email address.
+     */
     @Email(message = "Email was not provided")
     @Size(max = 80, message = "Email is too long")
     @Column(unique = true)
     private String email;
 
+    /**
+     * The user's first name.
+     */
     @NotNull(message = "FirstName must be present")
     @Size(min = 1, message = "Firstname cannot be empty")
     @Size(max = 30, message = "Firstname is too long")
     private String firstname;
 
-    @Size(max = 30, message = "Firstname is too long")
+    /**
+     * The user's last name.
+     */
+    @Size(max = 30, message = "Lastname is too long")
     private String lastname;
 
+    /**
+     * The user's gender.
+     */
     @Enumerated(EnumType.STRING)
     @JdbcTypeCode(SqlTypes.NAMED_ENUM)
     private Gender gender;
 
+    /**
+     * The user's sex.
+     */
     @Enumerated(EnumType.STRING)
     @JdbcTypeCode(SqlTypes.NAMED_ENUM)
     private Sex sex;
 
+    /**
+     * The user's address.
+     */
     @Embedded
     private Address address = new Address();
 
+    /**
+     * The user's role.
+     */
     @Enumerated(EnumType.STRING)
     @JdbcTypeCode(SqlTypes.NAMED_ENUM)
     private Role role;
 
+    /**
+     * The list of tokens associated with the user.
+     */
     @OneToMany(mappedBy = "user")
     private List<Token> tokens;
 
+    /**
+     * Return the authorities granted to the user.
+     *
+     * @return A collection of granted authorities.
+     */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return role.getAuthorities();
     }
 
+    /**
+     * Returns the password used to authenticate the user.
+     *
+     * @return The user's password.
+     */
     @Override
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return The user's email address.
+     */
     @Override
     public String getUsername() {
         return getEmail();
     }
 
+    /**
+     * Indicates whether the user's account has expired.
+     *
+     * @return {@code true} if the user's account is valid (i.e., not expired),
+     * {@code false} otherwise.
+     */
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    /**
+     * Indicates whether the user is locked or unlocked.
+     *
+     * @return {@code true} if the user is not locked, {@code false} otherwise.
+     */
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    /**
+     * Indicates whether the user's credentials (password) have expired.
+     *
+     * @return {@code true} if the user's credentials are valid (i.e., not expired),
+     * {@code false} otherwise.
+     */
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    /**
+     * Indicates whether the user is enabled or disabled.
+     *
+     * @return {@code true} if the user is enabled, {@code false} otherwise.
+     */
     @Override
     public boolean isEnabled() {
         return true;
