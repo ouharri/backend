d32a9c48dfbd66b75522f02aa71b14c5c7b1a456
ouharri <ouharrioutman@gmail.com>
2023-12-12 17:23:44 +0100

    Mapper interface for converting between Level DTOs and entities. Uses MapStruct for automatic mapping implementation.

--- src/main/java/com/ouharri/aftas/model/mapper/LevelMapper.java
+++ src/main/java/com/ouharri/aftas/model/mapper/LevelMapper.java
@@ -1,24 +1,24 @@
 package com.ouharri.aftas.model.mapper;
 
-import com.ouharri.aftas.model.dto.Level.LevelReq;
-import com.ouharri.aftas.model.dto.Level.LevelResp;
+import com.ouharri.aftas.model.dto.requests.LevelRequest;
+import com.ouharri.aftas.model.dto.responces.LevelResponse;
 import com.ouharri.aftas.model.entities.Level;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
-import org.springframework.data.domain.Page;
-import org.w3c.dom.stylesheets.LinkStyle;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
-import java.util.List;
+import java.util.UUID;
 
+/**
+ * Mapper interface for converting between {@link Level} DTOs and entities.
+ * Uses MapStruct for automatic mapping implementation.
+ */
 @Mapper(
-        componentModel = "spring",
-        injectionStrategy = InjectionStrategy.CONSTRUCTOR
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        componentModel = MappingConstants.ComponentModel.SPRING
 )
-public interface LevelMapper {
-    Level toEntity(LevelReq level);
-
-    List<Level> toEntity(List<LevelReq> levels);
-
-    LevelResp toResp(Level level);
+public interface LevelMapper extends _Mapper<UUID, LevelRequest, LevelResponse, Level> {
 
 }
