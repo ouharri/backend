68cda98ae8c1bbbe43599df60fbbe55bc8ba9b03
ouharri <ouharrioutman@gmail.com>
2023-12-12 17:08:23 +0100

    This class represents an address of User,Competition entities. The address includes information such as region, district, city, street, building number, apartment number, and postal code.

--- src/main/java/com/ouharri/aftas/model/entities/Address.java
+++ src/main/java/com/ouharri/aftas/model/entities/Address.java
@@ -9,7 +9,12 @@ import lombok.*;
 import java.util.Objects;
 
 /**
- * This class represents an address of {@link User} entities.
+ * This class represents an address of {@link User,Competition} entities.
+ *
+ * <p>The address includes information such as region, district, city, street, building number,
+ * apartment number, and postal code.
+ *
+ * <p>Instances of this class are embedded within {@link User,Competition} entities.
  *
  * @author Ouharri Outman
  * @version 1.0
@@ -22,24 +27,58 @@ import java.util.Objects;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Address {
+    /**
+     * The name of the region.
+     */
     @Size(max = 30, message = "Region name is too long")
     private String region;
+
+    /**
+     * The name of the district.
+     */
     @Size(max = 30, message = "District name is too long")
     private String district;
+
+    /**
+     * The name of the city.
+     */
     @Size(max = 30, message = "City name is too long")
     private String city;
+
+    /**
+     * The name of the street.
+     */
     @Size(max = 30, message = "Street name is too long")
     private String street;
+
+    /**
+     * The building number.
+     */
     @Min(value = 1, message = "Building number must be greater than 0")
     @Max(value = 32767, message = "Building number is too big")
     private Integer building;
+
+    /**
+     * The apartment number.
+     */
     @Min(value = 1, message = "Apartment number must be greater than 0")
     @Max(value = 32767, message = "Apartment number is too big")
     private Integer apartment;
+
+    /**
+     * The postal code.
+     */
     @Min(value = 1001, message = "Invalid postal code")
     @Max(value = 99999, message = "Invalid postal code")
     private Integer postalCode;
 
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     *
+     * @param o The reference object with which to compare.
+     * @return {@code true} if this object is the same as the o argument;
+     * {@code false} otherwise.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o)
