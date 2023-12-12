bfa83713ceb90bbadbecae5fa59b58fedca7c2f8
ouharri <ouharrioutman@gmail.com>
2023-12-12 18:15:35 +0100

    Mapper interface for converting between Member DTOs and entities. Uses MapStruct for automatic mapping implementation.

--- src/main/java/com/ouharri/aftas/model/mapper/MemberMapper.java
+++ src/main/java/com/ouharri/aftas/model/mapper/MemberMapper.java
@@ -3,6 +3,10 @@ package com.ouharri.aftas.model.mapper;
 import com.ouharri.aftas.model.dto.requests.MemberRequest;
 import com.ouharri.aftas.model.dto.responces.MemberResponse;
 import com.ouharri.aftas.model.entities.Member;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.UUID;
 
@@ -10,5 +14,11 @@ import java.util.UUID;
  * Mapper interface for converting between {@link Member} DTOs and entities.
  * Uses MapStruct for automatic mapping implementation.
  */
+@Mapper(
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        componentModel = MappingConstants.ComponentModel.SPRING
+)
 public interface MemberMapper extends _Mapper<UUID, MemberRequest, MemberResponse, Member> {
+
 }
