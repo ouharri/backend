6c62af193101420f4e636768797e716f967e70b4
ouharri <ouharrioutman@gmail.com>
2023-12-12 17:13:42 +0100

    Represents a token entity used for authentication and authorization.

--- src/main/java/com/ouharri/aftas/model/entities/Token.java
+++ src/main/java/com/ouharri/aftas/model/entities/Token.java
@@ -4,24 +4,45 @@ import com.ouharri.aftas.model.enums.TokenType;
 import jakarta.persistence.*;
 import lombok.*;
 
+/**
+ * Represents a token entity used for authentication and authorization.
+ *
+ * @author ouharri
+ * @version 2.0
+ */
 @Getter
 @Setter
 @Builder
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
 public class Token extends AbstractEntity {
-
+    
+    /**
+     * The token string, unique within the system.
+     */
     @Column(unique = true, length = 1000)
     public String token;
 
+    /**
+     * The type of the token, e.g., Bearer.
+     */
     @Enumerated(EnumType.STRING)
     public TokenType tokenType = TokenType.BEARER;
 
+    /**
+     * Indicates whether the token has been revoked.
+     */
     public boolean revoked;
 
+    /**
+     * Indicates whether the token has expired.
+     */
     public boolean expired;
 
+    /**
+     * The user associated with the token.
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     public User user;
