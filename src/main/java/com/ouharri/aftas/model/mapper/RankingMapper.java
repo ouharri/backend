4c14ab23b38791d2f202f7ba732e7fc51f4b70f4
ouharri <ouharrioutman@gmail.com>
2023-12-12 17:24:04 +0100

    Mapper interface for converting between Ranking DTOs and entities. Uses MapStruct for automatic mapping implementation.

--- src/main/java/com/ouharri/aftas/model/mapper/RankingMapper.java
+++ src/main/java/com/ouharri/aftas/model/mapper/RankingMapper.java
@@ -1,11 +1,23 @@
 package com.ouharri.aftas.model.mapper;
 
+import com.ouharri.aftas.model.dto.requests.RankingRequest;
+import com.ouharri.aftas.model.dto.responces.RankingResponse;
+import com.ouharri.aftas.model.entities.Ranking;
+import com.ouharri.aftas.model.entities.RankingId;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
+/**
+ * Mapper interface for converting between {@link Ranking} DTOs and entities.
+ * Uses MapStruct for automatic mapping implementation.
+ */
 @Mapper(
-        componentModel = "spring",
-        injectionStrategy = InjectionStrategy.CONSTRUCTOR
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        componentModel = MappingConstants.ComponentModel.SPRING
 )
-public interface RankingMapper {
+public interface RankingMapper extends _Mapper<RankingId, RankingRequest, RankingResponse, Ranking> {
+
 }
