6825c6dd07656eb6bb3e5321bcd154c576dbf9cb
ouharri <ouharrioutman@gmail.com>
2023-12-12 17:23:11 +0100

    Mapper interface for converting between Fish DTOs and entities. Uses MapStruct for automatic mapping implementation.

--- src/main/java/com/ouharri/aftas/model/mapper/FishMapper.java
+++ src/main/java/com/ouharri/aftas/model/mapper/FishMapper.java
@@ -1,18 +1,25 @@
 package com.ouharri.aftas.model.mapper;
 
-import com.ouharri.aftas.model.dto.Fish.FishReq;
-import com.ouharri.aftas.model.dto.Fish.FishResp;
+import com.ouharri.aftas.model.dto.requests.FishRequest;
+import com.ouharri.aftas.model.dto.responces.FishResponse;
+import com.ouharri.aftas.model.entities.Competition;
 import com.ouharri.aftas.model.entities.Fish;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
+import java.util.UUID;
+
+/**
+ * Mapper interface for converting between {@link Fish} DTOs and entities.
+ * Uses MapStruct for automatic mapping implementation.
+ */
 @Mapper(
-        componentModel = "spring",
-        injectionStrategy = InjectionStrategy.CONSTRUCTOR
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        componentModel = MappingConstants.ComponentModel.SPRING
 )
-public interface FishMapper {
+public interface FishMapper extends _Mapper<UUID,FishRequest, FishResponse, Fish> {
 
-    Fish toEntity(FishReq fish);
-
-    FishResp toResp(Fish fish);
 }
