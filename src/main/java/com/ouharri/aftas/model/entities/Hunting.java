a71244952ea7eb649fa7764751a28ad15d219297
ouharri <ouharrioutman@gmail.com>
2023-12-12 17:11:46 +0100

    Represents a record of a member's hunting activity in a competition. Extends the AbstractEntity class.

--- src/main/java/com/ouharri/aftas/model/entities/Hunting.java
+++ src/main/java/com/ouharri/aftas/model/entities/Hunting.java
@@ -6,9 +6,19 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
+/**
+ * Represents a record of a member's hunting activity in a competition.
+ * Extends the AbstractEntity class.
+ *
+ * @author ouharri
+ * @version 2.0
+ */
 @Entity
 @Getter
 @Setter
@@ -17,18 +27,31 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @Table(name = "hunting")
 public class Hunting extends AbstractEntity {
+
+    /**
+     * The number of fish caught during the hunting activity.
+     */
     @NotNull(message = "The number of fish cannot be null.")
     @Positive(message = "The number of fish must be a positive integer.")
-    private Integer numberOfFish;
+    private int numberOfFish;
 
+    /**
+     * The type of fish hunted.
+     */
     @ManyToOne
     @JoinColumn(name = "fish_id", referencedColumnName = "id")
     private Fish fish;
 
+    /**
+     * The member who participated in the hunting activity.
+     */
     @ManyToOne
     @JoinColumn(name = "member_id", referencedColumnName = "id")
     private Member member;
 
+    /**
+     * The competition in which the hunting activity took place.
+     */
     @ManyToOne
     @JoinColumn(name = "competition_id", referencedColumnName = "id")
     private Competition competition;
