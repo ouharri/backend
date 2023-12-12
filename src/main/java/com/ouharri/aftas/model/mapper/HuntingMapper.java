977b2ef74622bbbe4c9a3375fd2662a24cd8f506
ouharri <ouharrioutman@gmail.com>
2023-12-12 17:23:26 +0100

    Mapper interface for converting between Hunting DTOs and entities. Uses MapStruct for automatic mapping implementation.

--- src/main/java/com/ouharri/aftas/model/mapper/HuntingMapper.java
+++ src/main/java/com/ouharri/aftas/model/mapper/HuntingMapper.java
@@ -1,11 +1,23 @@
 package com.ouharri.aftas.model.mapper;
 
+import com.ouharri.aftas.model.dto.requests.HuntingRequest;
+import com.ouharri.aftas.model.dto.responces.HuntingResponse;
+import com.ouharri.aftas.model.entities.Hunting;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
+import java.util.UUID;
+
+/**
+ * Mapper interface for converting between {@link Hunting} DTOs and entities.
+ * Uses MapStruct for automatic mapping implementation.
+ */
 @Mapper(
-        componentModel = "spring",
-        injectionStrategy = InjectionStrategy.CONSTRUCTOR
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        componentModel = MappingConstants.ComponentModel.SPRING
 )
-public interface HuntingMapper {
+public interface HuntingMapper extends _Mapper<UUID, HuntingRequest, HuntingResponse, Hunting> {
 }
