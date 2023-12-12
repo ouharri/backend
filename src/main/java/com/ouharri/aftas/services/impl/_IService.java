f16f7a9b253cf878da3b406ad8ce785180ceaf42
ouharri <ouharrioutman@gmail.com>
2023-12-12 17:46:26 +0100

    Generic service implementation with common CRUD operations.

--- src/main/java/com/ouharri/aftas/services/impl/_IService.java
+++ src/main/java/com/ouharri/aftas/services/impl/_IService.java
@@ -1,9 +1,9 @@
 package com.ouharri.aftas.services.impl;
 
 import com.ouharri.aftas.exceptions.ResourceNotCreatedException;
-import com.ouharri.aftas.model.dto.requests.Request;
-import com.ouharri.aftas.model.dto.responces.Response;
-import com.ouharri.aftas.model.entities.AbstractEntity;
+import com.ouharri.aftas.model.dto.requests._Request;
+import com.ouharri.aftas.model.dto.responces._Response;
+import com.ouharri.aftas.model.entities._Entity;
 import com.ouharri.aftas.model.mapper._Mapper;
 import com.ouharri.aftas.services.spec._service;
 import lombok.AllArgsConstructor;
@@ -17,7 +17,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 
 import java.util.Optional;
-import java.util.UUID;
 
 /**
  * Generic service implementation with common CRUD operations.
@@ -32,11 +31,13 @@ import java.util.UUID;
 @Validated
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
-public abstract class _IService<Req extends Request, Res extends Response, Entity extends AbstractEntity, Repository extends JpaRepository<Entity, UUID>, Mapper extends _Mapper<Req, Res, Entity>> implements _service<Req, Res> {
+public abstract class _IService<ID, Req extends _Request, Res extends _Response, Entity extends _Entity<ID>, Repository extends JpaRepository<Entity, ID>, Mapper extends _Mapper<ID, Req, Res, Entity>> implements _service<ID, Req, Res> {
 
     @Autowired
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     protected final Repository repository;
     @Autowired
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     protected final Mapper mapper;
 
     /**
@@ -98,7 +99,7 @@ public abstract class _IService<Req extends Request, Res extends Response, Entit
      * @param id Unique identifier of the entity.
      * @return Optional containing the response DTO of the found entity.
      */
-    public Optional<Res> getById(UUID id) {
+    public Optional<Res> getById(ID id) {
         assert repository != null;
         assert mapper != null;
         return repository.findById(id)
