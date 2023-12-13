a3e437ea3cc7ac07dbcfc4ac90dd9cd92701da3c
ouharri <ouharrioutman@gmail.com>
2023-12-13 02:20:02 +0100

    DTO for Member

--- src/main/java/com/ouharri/aftas/model/dto/responces/MemberResponse.java
+++ src/main/java/com/ouharri/aftas/model/dto/responces/MemberResponse.java
@@ -1,33 +1,44 @@
 package com.ouharri.aftas.model.dto.responces;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.ouharri.aftas.model.entities.Member;
 import com.ouharri.aftas.model.enums.IdentityDocumentType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import java.sql.Date;
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 /**
- * DTO for {@link com.ouharri.aftas.model.entities.Member}
+ * DTO for {@link Member}
  */
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record MemberResponse(
-        UUID id,
-        String createdAt,
-        LocalDateTime updatedAt,
-        @NotBlank(message = "Name cannot be blank.")
-        String name,
-        @NotBlank(message = "Family name cannot be blank.")
-        String familyName,
-        @NotNull(message = "Accession date cannot be null.")
-        Date accessionDate,
-        @NotBlank(message = "Nationality cannot be blank.")
-        String nationality,
-        @NotNull(message = "Identity document type cannot be null.")
-        IdentityDocumentType identityDocument,
-        @NotBlank(message = "Identity number cannot be blank.")
-        String identityNumber
-) implements Response {
+public class MemberResponse extends AbstractResponse {
+
+    @NotBlank(message = "Name cannot be blank.")
+    private String name;
+
+    @NotBlank(message = "Family name cannot be blank.")
+    private String familyName;
+
+    @NotNull(message = "Accession date cannot be null.")
+    private Date accessionDate;
+
+    @NotBlank(message = "Nationality cannot be blank.")
+    private String nationality;
+
+    @NotNull(message = "Identity document type cannot be null.")
+    private IdentityDocumentType identityDocument;
+
+    @NotBlank(message = "Identity number cannot be blank.")
+    private String identityNumber;
 }
