bec5afe171065798388db8418adbd0879d7f0abc
ouharri <ouharrioutman@gmail.com>
2023-12-13 12:24:15 +0100

    Interface for validating date and time fields.

---
+++ src/main/java/com/ouharri/aftas/validations/interfaces/DateTimeValidator.java
@@ -0,0 +1,41 @@
+package com.ouharri.aftas.validations.interfaces;
+
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotNull;
+
+import java.sql.Time;
+import java.util.Date;
+
+/**
+ * Interface for validating date and time fields.
+ */
+public interface DateTimeValidator {
+
+    /**
+     * Gets the date value to be validated.
+     *
+     * @return The date value.
+     */
+    @NotNull(message = "The date cannot be null.")
+    @Temporal(TemporalType.DATE)
+    Date getDate();
+
+    /**
+     * Gets the start time value to be validated.
+     *
+     * @return The start time value.
+     */
+    @NotNull(message = "The start time cannot be null.")
+    @Temporal(TemporalType.TIME)
+    Time getStartTime();
+
+    /**
+     * Gets the end time value to be validated.
+     *
+     * @return The end time value.
+     */
+    @NotNull(message = "The end time cannot be null.")
+    @Temporal(TemporalType.TIME)
+    Time getEndTime();
+}
