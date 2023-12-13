aae4f2099d9fb39d9e4cb965a992e545f58a9656
ouharri <ouharrioutman@gmail.com>
2023-12-13 12:25:50 +0100

    Validator for the FutureDate constraint. Ensures that the provided date is in the future.

---
+++ src/main/java/com/ouharri/aftas/validations/validator/FutureDateValidator.java
@@ -0,0 +1,32 @@
+package com.ouharri.aftas.validations.validator;
+
+import com.ouharri.aftas.validations.Annotation.FutureDate;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.sql.Date;
+
+/**
+ * Validator for the {@link FutureDate} constraint.
+ * Ensures that the provided date is in the future.
+ */
+public class FutureDateValidator implements ConstraintValidator<FutureDate, Date> {
+
+    /**
+     * Validates whether the provided date is in the future.
+     *
+     * @param date    The date to be validated.
+     * @param context The validation context.
+     * @return {@code true} if the date is in the future or {@code null}, {@code false} otherwise.
+     */
+    @Override
+    public boolean isValid(Date date, ConstraintValidatorContext context) {
+        if (date == null) {
+            return true;
+        }
+
+        Date currentDate = new Date(System.currentTimeMillis());
+
+        return date.after(currentDate);
+    }
+}
