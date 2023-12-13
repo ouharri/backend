5bf55e5192db22a68eca16504cb14c5dc1a08f0d
ouharri <ouharrioutman@gmail.com>
2023-12-13 02:23:58 +0100

    Mapper interface for converting between Ranking DTOs and entities. Uses MapStruct for automatic mapping implementation.

--- src/main/java/com/ouharri/aftas/services/spec/RankingService.java
+++ src/main/java/com/ouharri/aftas/services/spec/RankingService.java
@@ -1,8 +1,10 @@
 package com.ouharri.aftas.services.spec;
 
-import org.springframework.stereotype.Service;
+import com.ouharri.aftas.model.dto.requests.RankingRequest;
+import com.ouharri.aftas.model.dto.responces.RankingResponse;
+import com.ouharri.aftas.model.entities.Hunting;
+import com.ouharri.aftas.model.entities.RankingId;
 
-
-@Service
-public interface RankingService {
+public interface RankingService extends _Service<RankingId,RankingRequest, RankingResponse> {
+    void calculateAndSetRanking(Hunting hunting);
 }
