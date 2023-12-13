92b5f56f5b0a7fc7495489a8f12778b130dbb6f9
ouharri <ouharrioutman@gmail.com>
2023-12-13 02:31:11 +0100

    Hunting Repository

--- src/main/java/com/ouharri/aftas/repositories/HuntingRepository.java
+++ src/main/java/com/ouharri/aftas/repositories/HuntingRepository.java
@@ -1,6 +1,7 @@
 package com.ouharri.aftas.repositories;
 
 import com.ouharri.aftas.model.entities.Hunting;
+import com.ouharri.aftas.model.entities.HuntingCompositeKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,5 @@ import java.util.UUID;
 
 @Repository
 public interface HuntingRepository extends JpaRepository<Hunting, UUID> {
+    Boolean existsByHuntingCompositeKey(HuntingCompositeKey huntingCompositeKey);
 }
