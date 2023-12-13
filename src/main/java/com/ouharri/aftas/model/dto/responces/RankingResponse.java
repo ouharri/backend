c1228988e54966125667b1e3d7d7b9d3186cdb25
ouharri <ouharrioutman@gmail.com>
2023-12-13 02:20:27 +0100

    DTO for com.ouharri.aftas.model.entities.Ranking

--- src/main/java/com/ouharri/aftas/model/dto/responces/RankingResponse.java
+++ src/main/java/com/ouharri/aftas/model/dto/responces/RankingResponse.java
@@ -3,19 +3,21 @@ package com.ouharri.aftas.model.dto.responces;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotNull;
 
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
 
 /**
  * DTO for {@link com.ouharri.aftas.model.entities.Ranking}
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record RankingResponse(
-        RankingIdResponse id,
+        CompetitionResponse competition,
+        MemberResponse member,
+
         @NotNull(message = "Score cannot be null.")
         Long score,
         @NotNull(message = "Rank cannot be null.")
         Integer rank,
-        LocalDateTime createdAt,
-        LocalDateTime updatedAt
-) implements Response {
+        Timestamp createdAt,
+        Timestamp updatedAt
+) implements _Response {
 }
