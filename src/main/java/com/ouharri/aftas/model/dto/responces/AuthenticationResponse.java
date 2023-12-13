fae5305b5ec77acfc992dbcb84c8facbc013ab93
ouharri <ouharrioutman@gmail.com>
2023-12-13 02:19:18 +0100

    DTO class representing the response for authentication. Contains access and refresh tokens.

--- src/main/java/com/ouharri/aftas/model/dto/responces/AuthenticationResponse.java
+++ src/main/java/com/ouharri/aftas/model/dto/responces/AuthenticationResponse.java
@@ -1,13 +1,11 @@
 package com.ouharri.aftas.model.dto.responces;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 /**
  * DTO class representing the response for authentication.
@@ -20,18 +18,18 @@ import jakarta.validation.constraints.Size;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthenticationResponse implements Response {
-  /**
-   * Access token for authentication.
-   */
-  @NotBlank(message = "Access token is required")
-  @JsonProperty("access_token")
-  private String accessToken;
+public class AuthenticationResponse implements _Response {
+    /**
+     * Access token for authentication.
+     */
+    @NotBlank(message = "Access token is required")
+    @JsonProperty("access_token")
+    private String accessToken;
 
-  /**
-   * Refresh token for token refreshing.
-   */
-  @NotBlank(message = "Refresh token is required")
-  @JsonProperty("refresh_token")
-  private String refreshToken;
+    /**
+     * Refresh token for token refreshing.
+     */
+    @NotBlank(message = "Refresh token is required")
+    @JsonProperty("refresh_token")
+    private String refreshToken;
 }
