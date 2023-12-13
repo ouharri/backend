39dbeea74cbce39ac564ba92006050ffa448696a
ouharri <ouharrioutman@gmail.com>
2023-12-13 02:28:11 +0100

    Represents a type of fish that can be hunted in competitions. Extends the AbstractEntity class.

--- src/main/java/com/ouharri/aftas/model/entities/Fish.java
+++ src/main/java/com/ouharri/aftas/model/entities/Fish.java
@@ -4,7 +4,10 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 import java.util.ArrayList;
