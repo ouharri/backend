527488cc4e3dfb3c6f137a5636d94ff539bb2f1a
ouharri <ouharrioutman@gmail.com>
2023-12-13 10:04:12 +0100

    Repository for Hunting entity

--- src/main/java/com/ouharri/aftas/repositories/HuntingRepository.java
+++ src/main/java/com/ouharri/aftas/repositories/HuntingRepository.java
@@ -3,6 +3,8 @@ package com.ouharri.aftas.repositories;
 import com.ouharri.aftas.model.entities.Hunting;
 import com.ouharri.aftas.model.entities.HuntingCompositeKey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -10,4 +12,10 @@ import java.util.UUID;
 @Repository
 public interface HuntingRepository extends JpaRepository<Hunting, UUID> {
     Boolean existsByHuntingCompositeKey(HuntingCompositeKey huntingCompositeKey);
+
+    @Query("SELECT SUM(f.level.points * h.numberOfFish) " +
+            "FROM Hunting h " +
+            "JOIN h.huntingCompositeKey.fish f " +
+            "WHERE h.huntingCompositeKey.member.id = :memberId AND h.huntingCompositeKey.competition.id = :competitionId")
+    Long calculateMemberPoints(@Param("memberId") UUID memberId, @Param("competitionId") UUID competitionId);
 }
