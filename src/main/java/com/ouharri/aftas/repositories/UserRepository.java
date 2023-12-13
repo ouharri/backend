bf249be0b1568eea00f882b1874510fa17415ce4
ouharri <ouharrioutman@gmail.com>
2023-12-13 02:32:04 +0100

    User Repository

--- src/main/java/com/ouharri/aftas/repositories/UserRepository.java
+++ src/main/java/com/ouharri/aftas/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.ouharri.aftas.repositories;
 
 import com.ouharri.aftas.model.entities.User;
+import io.micrometer.common.lang.NonNullApi;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,9 +9,11 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
+@NonNullApi
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-  Optional<User> findByEmail(String email);
-  Optional<User> findById(UUID id);
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findById(UUID id);
 
 }
