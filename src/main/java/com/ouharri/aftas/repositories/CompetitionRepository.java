f6e710d141370cf2ed692e62bc14cba83a558fe4
ouharri <ouharrioutman@gmail.com>
2023-12-14 20:10:14 +0100

    Repository interface for accessing and managing Competition entities in the database.

--- src/main/java/com/ouharri/aftas/repositories/CompetitionRepository.java
+++ src/main/java/com/ouharri/aftas/repositories/CompetitionRepository.java
@@ -8,12 +8,27 @@ import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+/**
+ * Repository interface for accessing and managing {@link Competition} entities in the database.
+ */
 @Repository
 public interface CompetitionRepository extends JpaRepository<Competition, UUID> {
 
-    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Competition c WHERE c.id = :id AND c.endTime < CURRENT_TIME")
-    boolean hasCompetitionEnded(@Param("id") UUID id);
+    /**
+     * Checks if a competition with the specified ID has not started yet.
+     *
+     * @param id The ID of the competition to check.
+     * @return true if the competition has not started, false otherwise.
+     */
+    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Competition c WHERE c.id = :id AND (CURRENT_DATE < c.date AND CURRENT_TIME < c.startTime)")
+    boolean hasCompetitionNotStarted(@Param("id") UUID id);
 
-    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Competition c WHERE c.id = :id AND CURRENT_DATE > c.date OR (CURRENT_DATE = c.date AND CURRENT_TIME > c.startTime)")
-    boolean hasCompetitionStarted(@Param("id") UUID id);
+    /**
+     * Checks if a competition with the specified ID has ended.
+     *
+     * @param id The ID of the competition to check.
+     * @return true if the competition has ended, false otherwise.
+     */
+    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Competition c WHERE c.id = :id AND (CURRENT_DATE > c.date or (CURRENT_DATE = c.date AND CURRENT_TIME > c.endTime))")
+    boolean hasCompetitionEnded(@Param("id") UUID id);
 }
