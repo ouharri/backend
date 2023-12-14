561db0fbda50851851a5f62db8cd72159f087b60
ouharri <ouharrioutman@gmail.com>
2023-12-14 20:42:29 +0100

    Repository interface for accessing and managing Token entities in the database.

--- src/main/java/com/ouharri/aftas/repositories/TokenRepository.java
+++ src/main/java/com/ouharri/aftas/repositories/TokenRepository.java
@@ -9,15 +9,30 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+/**
+ * Repository interface for accessing and managing {@link Token} entities in the database.
+ */
 @Repository
 public interface TokenRepository extends JpaRepository<Token, Integer> {
 
+    /**
+     * Finds all valid tokens associated with a user based on the user's ID.
+     *
+     * @param id The ID of the user.
+     * @return A list of valid tokens associated with the user.
+     */
     @Query(value = """
-            select t from Token t inner join User u\s
-            on t.user.id = u.id\s
-            where u.id = :id and (t.expired = false or t.revoked = false)\s
+            SELECT t FROM Token t INNER JOIN User u
+            ON t.user.id = u.id
+            WHERE u.id = :id AND (t.expired = false OR t.revoked = false)
             """)
     List<Token> findAllValidTokenByUser(UUID id);
 
+    /**
+     * Finds a token by its token string.
+     *
+     * @param token The token string.
+     * @return An optional containing the token, or empty if not found.
+     */
     Optional<Token> findByToken(String token);
 }
