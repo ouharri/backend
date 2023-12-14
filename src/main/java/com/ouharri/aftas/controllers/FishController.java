0222da9e85ff0bc8dfb9b263359b885853965cab
ouharri <ouharrioutman@gmail.com>
2023-12-14 22:44:15 +0100

    Controller class for handling CRUD operations on Fish entities. Exposes RESTful endpoints for managing Fish entities.

--- src/main/java/com/ouharri/aftas/controllers/FishController.java
+++ src/main/java/com/ouharri/aftas/controllers/FishController.java
@@ -1,43 +1,25 @@
 package com.ouharri.aftas.controllers;
 
-import com.ouharri.aftas.exceptions.ResourceNotCreatedException;
-import com.ouharri.aftas.model.dto.Fish.FishReq;
-import com.ouharri.aftas.model.dto.Fish.FishResp;
+import com.ouharri.aftas.model.dto.requests.FishRequest;
+import com.ouharri.aftas.model.dto.responces.FishResponse;
 import com.ouharri.aftas.services.spec.FishService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.UUID;
 
+/**
+ * Controller class for handling CRUD operations on Fish entities.
+ * Exposes RESTful endpoints for managing Fish entities.
+ *
+ * @see _Controller
+ */
 @Slf4j
 @Validated
 @RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/v2/fish")
-public class FishController {
-    private final FishService fishService;
+@RequestMapping("api/v2/fish")
+public class FishController extends _Controller<UUID, FishRequest, FishResponse, FishService> {
 
-    @PostMapping
-    public ResponseEntity<FishResp> createLevel(
-            @Valid @RequestBody FishReq fish,
-            BindingResult bindingResult
-    ) {
-        System.out.println("\n\n\n");
-        System.out.println(fish);
-        if (bindingResult.hasErrors())
-            throw new ResourceNotCreatedException(bindingResult);
-
-        Optional<FishResp> fishCreated = fishService.createFish(fish);
-
-        return fishCreated.map(ResponseEntity::ok).orElseGet(() ->
-                ResponseEntity.badRequest().build());
-    }
 }
