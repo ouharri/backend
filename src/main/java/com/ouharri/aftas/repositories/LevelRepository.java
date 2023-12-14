acd57055c3371084bdb06cb85ef61afe51648250
ouharri <ouharrioutman@gmail.com>
2023-12-14 20:09:50 +0100

    Repository interface for accessing and managing {@link Level} entities in the database.

--- src/main/java/com/ouharri/aftas/repositories/LevelRepository.java
+++ src/main/java/com/ouharri/aftas/repositories/LevelRepository.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+/**
+ * Repository interface for accessing and managing {@link Level} entities in the database.
+ */
 @Repository
 public interface LevelRepository extends JpaRepository<Level, UUID> {
 }
