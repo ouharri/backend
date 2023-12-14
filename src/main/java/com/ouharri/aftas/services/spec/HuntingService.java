25a4593e702ae0bd6eff9de3cb854c518871b8fa
ouharri <ouharrioutman@gmail.com>
2023-12-14 20:48:45 +0100

    Service interface for managing Hunting entities. Extends the generic service interface _Service.

--- src/main/java/com/ouharri/aftas/services/spec/HuntingService.java
+++ src/main/java/com/ouharri/aftas/services/spec/HuntingService.java
@@ -1,7 +1,32 @@
 package com.ouharri.aftas.services.spec;
 
-import org.springframework.stereotype.Service;
+import com.ouharri.aftas.model.dto.requests.HuntingRequest;
+import com.ouharri.aftas.model.dto.responces.HuntingResponse;
 
-@Service
-public interface HuntingService {
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Service interface for managing Hunting entities.
+ * Extends the generic service interface {@link _Service}.
+ *
+ * @see _Service
+ */
+public interface HuntingService extends _Service<UUID, HuntingRequest, HuntingResponse> {
+
+    /**
+     * Increments the number of fish for a hunting record and returns the updated response.
+     *
+     * @param request The hunting response DTO containing the data to be updated.
+     * @return An {@link Optional} containing the updated hunting response, or an empty {@link Optional} if the operation fails.
+     */
+    Optional<HuntingResponse> incrementNumberOfFish(HuntingResponse request);
+
+    /**
+     * Decrements the number of fish for a hunting record and returns the updated response.
+     *
+     * @param request The hunting response DTO containing the data to be updated.
+     * @return An {@link Optional} containing the updated hunting response, or an empty {@link Optional} if the operation fails.
+     */
+    Optional<HuntingResponse> decrementNumberOfFish(HuntingResponse request);
 }
