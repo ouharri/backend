4f2ecedd0e4bc55c8ec71ddf9165a0d76b23f740
ouharri <ouharrioutman@gmail.com>
2023-12-14 22:38:35 +0100

    Implementation of the LevelService interface.

---
+++ src/main/java/com/ouharri/aftas/services/impl/LevelServiceImp.java
@@ -0,0 +1,21 @@
+package com.ouharri.aftas.services.impl;
+
+import com.ouharri.aftas.model.dto.requests.LevelRequest;
+import com.ouharri.aftas.model.dto.responces.LevelResponse;
+import com.ouharri.aftas.model.entities.Level;
+import com.ouharri.aftas.model.mapper.LevelMapper;
+import com.ouharri.aftas.repositories.LevelRepository;
+import com.ouharri.aftas.services.spec.LevelService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+/**
+ * Implementation of the {@link LevelService} interface.
+ */
+@Slf4j
+@Service
+public class LevelServiceImp extends _ServiceImp<UUID, LevelRequest, LevelResponse, Level, LevelRepository, LevelMapper> implements LevelService {
+
+}
