3148621f3f8b55725e1f8849b701c77cfbf24da9
ouharri <ouharrioutman@gmail.com>
2023-12-14 20:43:43 +0100

    Repository interface for accessing and managing User entities in the database.

--- src/main/java/com/ouharri/aftas/repositories/UserRepository.java
+++ src/main/java/com/ouharri/aftas/repositories/UserRepository.java
@@ -8,12 +8,26 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import java.util.UUID;
 
+/**
+ * Repository interface for accessing and managing {@link User} entities in the database.
+ */
 @Repository
 @NonNullApi
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+    /**
+     * Finds a user by their email address.
+     *
+     * @param email The email address of the user.
+     * @return An optional containing the user, or empty if not found.
+     */
     Optional<User> findByEmail(String email);
 
+    /**
+     * Finds a user by their ID.
+     *
+     * @param id The ID of the user.
+     * @return An optional containing the user, or empty if not found.
+     */
     Optional<User> findById(UUID id);
-
 }
