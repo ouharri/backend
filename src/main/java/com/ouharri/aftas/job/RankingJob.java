4267acb453ae2fb378e9d46349771aa3123bde91
ouharri <ouharrioutman@gmail.com>
2023-12-14 09:21:53 +0100

    Quartz Job for calculating rankings for a competition when the ends.

---
+++ src/main/java/com/ouharri/aftas/job/RankingJob.java
@@ -0,0 +1,55 @@
+package com.ouharri.aftas.job;
+
+import com.ouharri.aftas.model.entities.Competition;
+import com.ouharri.aftas.model.mapper.CompetitionMapper;
+import com.ouharri.aftas.services.spec.CompetitionService;
+import com.ouharri.aftas.services.spec.RankingService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.Job;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.UUID;
+
+
+/**
+ * Quartz Job for calculating rankings for a competition when the ends.
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RankingJob implements Job {
+    private final CompetitionMapper competitionMapper;
+
+    private final RankingService rankingService;
+    private final CompetitionService competitionService;
+
+    /**
+     * Executes the ranking calculation job.
+     *
+     * @param jobExecutionContext The execution context of the Quartz job.
+     * @throws JobExecutionException If an error occurs during job execution.
+     */
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        try {
+            JobDataMap jobDataMap = jobExecutionContext.getMergedJobDataMap();
+            Competition competition = competitionMapper.toEntityFromResponse(competitionService.getById(
+                            (UUID) jobDataMap.get("competitionID")
+                    ).orElseThrow(
+                            () -> new RuntimeException("Competition not found")
+                    )
+            );
+            rankingService.calculateRanking(competition);
+            String jobId = (String) jobDataMap.get("jobID");
+            log.info("Job Started-" + jobId + " at:" + new Date() + " for competition:" + competition.getId());
+        } catch (Exception e) {
+            log.error("Error while executing job", e);
+            throw new JobExecutionException(e.getMessage());
+        }
+    }
+}
