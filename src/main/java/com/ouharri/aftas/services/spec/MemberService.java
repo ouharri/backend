fb3552dfa3619d9c815238410480ea6429f04851
ouharri <ouharrioutman@gmail.com>
2023-12-14 20:50:26 +0100

    Service interface for managing Member entities. Extends the generic service interface _Service.

--- src/main/java/com/ouharri/aftas/services/spec/MemberService.java
+++ src/main/java/com/ouharri/aftas/services/spec/MemberService.java
@@ -1,7 +1,16 @@
 package com.ouharri.aftas.services.spec;
 
-import org.springframework.stereotype.Service;
+import com.ouharri.aftas.model.dto.requests.MemberRequest;
+import com.ouharri.aftas.model.dto.responces.MemberResponse;
 
-@Service
-public interface MemberService {
+import java.util.UUID;
+
+/**
+ * Service interface for managing Member entities.
+ * Extends the generic service interface {@link _Service}.
+ *
+ * @see _Service
+ */
+public interface MemberService extends _Service<UUID, MemberRequest, MemberResponse> {
+
 }
