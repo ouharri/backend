35e3b1c8dbb4c9f6d4d8be8fd7f47bbf1cbee6e9
ouharri <ouharrioutman@gmail.com>
2023-12-14 22:47:09 +0100

    Controller class for handling CRUD operations on Hunting entities. Exposes RESTful endpoints for managing Hunting entities and provides specific endpoints for incrementing and decrementing the number of fish in a hunting session.

--- src/main/java/com/ouharri/aftas/controllers/HuntingController.java
+++ src/main/java/com/ouharri/aftas/controllers/HuntingController.java
@@ -1,17 +1,79 @@
 package com.ouharri.aftas.controllers;
 
+import com.ouharri.aftas.model.dto.requests.HuntingRequest;
+import com.ouharri.aftas.model.dto.responces.HuntingResponse;
 import com.ouharri.aftas.services.spec.HuntingService;
-import lombok.RequiredArgsConstructor;
+import jakarta.validation.Valid;
+import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Controller class for handling CRUD operations on Hunting entities.
+ * Exposes RESTful endpoints for managing Hunting entities and provides
+ * specific endpoints for incrementing and decrementing the number of fish in a hunting session.
+ *
+ * @see _Controller
+ */
 @Slf4j
+@Builder
 @Validated
 @RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/v2/auth")
-public class HuntingController {
-    private final HuntingService huntingService;
+@RequestMapping("/api/v2/hunting")
+public class HuntingController extends _Controller<UUID, HuntingRequest, HuntingResponse, HuntingService> {
+
+    /**
+     * Endpoint for incrementing the number of fish in a hunting session.
+     *
+     * @param request       The request DTO.
+     * @param bindingResult The result of the validation.
+     * @return ResponseEntity containing the updated hunting entity or a bad request if the operation fails.
+     */
+    @PutMapping("fish/increment")
+    private ResponseEntity<HuntingResponse> incrementNumberOfFish(
+            @Valid @RequestBody HuntingResponse request,
+            BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors())
+            handleValidationError(bindingResult);
+
+        Optional<HuntingResponse> response = getService().incrementNumberOfFish(request);
+
+        return response.map(ResponseEntity::ok)
+                .orElseGet(() ->
+                        ResponseEntity.badRequest().build()
+                );
+    }
+
+    /**
+     * Endpoint for decrementing the number of fish in a hunting session.
+     *
+     * @param request       The request DTO.
+     * @param bindingResult The result of the validation.
+     * @return ResponseEntity containing the updated hunting entity or a bad request if the operation fails.
+     */
+    @PutMapping("fish/decrement")
+    private ResponseEntity<HuntingResponse> decrementNumberOfFish(
+            @Valid @RequestBody HuntingResponse request,
+            BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors())
+            handleValidationError(bindingResult);
+
+        Optional<HuntingResponse> response = getService().decrementNumberOfFish(request);
+
+        return response.map(ResponseEntity::ok)
+                .orElseGet(() ->
+                        ResponseEntity.badRequest().build()
+                );
+    }
 }
