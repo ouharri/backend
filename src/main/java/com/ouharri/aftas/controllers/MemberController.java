a062377e7cf512686ed308e8df2775c973db5f69
ouharri <ouharrioutman@gmail.com>
2024-02-24 17:24:36 +0000

    Controller class for handling CRUD operations on Member entities. Exposes RESTful endpoints for managing Member entities, including creation, retrieval, update, and deletion.

--- src/main/java/com/ouharri/aftas/controllers/MemberController.java
+++ src/main/java/com/ouharri/aftas/controllers/MemberController.java
@@ -1,7 +1,7 @@
 package com.ouharri.aftas.controllers;
 
 import com.ouharri.aftas.model.dto.requests.MemberRequest;
-import com.ouharri.aftas.model.dto.responces.MemberResponse;
+import com.ouharri.aftas.model.dto.responses.MemberResponse;
 import com.ouharri.aftas.services.spec.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,13 +15,14 @@ import java.util.UUID;
  * Controller class for handling CRUD operations on Member entities.
  * Exposes RESTful endpoints for managing Member entities, including creation, retrieval, update, and deletion.
  *
+ * @author <a href="mailto:ouharrioutman@gmail.com">ouharri outman</a>
  * @see _Controller
  */
 @Slf4j
 @Validated
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v2/member")
+@RequestMapping("/api/v2/members")
 public class MemberController extends _Controller<UUID, MemberRequest, MemberResponse, MemberService> {
 
 }
