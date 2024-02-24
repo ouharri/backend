84b0af58ca39793b4119a216ace5827ef275ec5a
ouharri <ouharrioutman@gmail.com>
2024-02-24 17:03:16 +0000

    Abstract base class for entities in the Aftas application. Provides common fields such as id, creation timestamp, and update timestamp. Subclasses should use the @MappedSuperclass annotation.

--- src/main/java/com/ouharri/aftas/model/entities/AbstractEntity.java
+++ src/main/java/com/ouharri/aftas/model/entities/AbstractEntity.java
@@ -12,6 +12,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.ReadOnlyProperty;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.io.Serial;
 import java.sql.Timestamp;
 import java.util.UUID;
 
@@ -20,7 +21,7 @@ import java.util.UUID;
  * Provides common fields such as id, creation timestamp, and update timestamp.
  * Subclasses should use the @MappedSuperclass annotation.
  *
- * @author Ouharri Outman
+ * @author <a href="mailto:ouharri.outman@gmail.com">ouharri</a>
  * @version 1.0
  */
 @Getter
@@ -29,40 +30,13 @@ import java.util.UUID;
 @MappedSuperclass
 @NoArgsConstructor
 @AllArgsConstructor
-public abstract class AbstractEntity implements _Entity<UUID> {
+public abstract class AbstractEntity<ID> extends AuditableEntity implements _Entity<ID> {
 
     /**
      * The unique identifier for the entity.
      */
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    private ID id;
 
-    /**
-     * The timestamp indicating when the entity was created.
-     */
-    @CreationTimestamp
-    @ReadOnlyProperty
-    @Temporal(TemporalType.TIMESTAMP)
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private Timestamp createdAt;
-
-    /**
-     * The timestamp indicating when the entity was last updated.
-     */
-    @UpdateTimestamp
-    @ReadOnlyProperty
-    @LastModifiedDate
-    @Column(name = "updated_at")
-    @Temporal(TemporalType.TIMESTAMP)
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private Timestamp updatedAt;
-
-    /**
-     * The version of the entity, used for optimistic locking.
-     */
-    @Version
-    @ReadOnlyProperty
-    private Long version = 0L;
 }
