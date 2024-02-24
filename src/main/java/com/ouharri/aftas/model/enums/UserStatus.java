eb411b77a75d8e88db21ca1d7b37d20e74d71d3e
ouharri <ouharrioutman@gmail.com>
2024-02-24 17:09:51 +0000

    Enumeration representing the status of a user (e.g., online or offline).

---
+++ src/main/java/com/ouharri/aftas/model/enums/UserStatus.java
@@ -0,0 +1,18 @@
+package com.ouharri.aftas.model.enums;
+
+/**
+ * Enumeration representing the status of a user (e.g., online or offline).
+ *
+ * @version 1.0
+ */
+public enum UserStatus {
+    /**
+     * Represents the status when a user is online.
+     */
+    ONLINE,
+
+    /**
+     * Represents the status when a user is offline.
+     */
+    OFFLINE
+}
