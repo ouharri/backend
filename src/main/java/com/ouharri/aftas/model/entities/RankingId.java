bee96c53485096b01364d3d1818615a4d4a7d903
ouharri <ouharrioutman@gmail.com>
2024-02-24 17:47:34 +0000

    Represents the composite key for the Ranking entity, consisting of the associated Competition and Member. Implements Serializable and Embeddable for use as an embedded key in the Ranking entity.

--- src/main/java/com/ouharri/aftas/model/entities/RankingId.java
+++ src/main/java/com/ouharri/aftas/model/entities/RankingId.java
@@ -11,12 +11,13 @@ import java.io.Serializable;
  * Represents the composite key for the Ranking entity, consisting of the associated Competition and Member.
  * Implements Serializable and Embeddable for use as an embedded key in the Ranking entity.
  *
- * @author ouharri
+ * @author <a href="mailto:ouharrioutman@gmail.com">Ouharri Outman</a>
  * @version 2.0
  */
 @Getter
 @Setter
 @Builder
+@ToString
 @Embeddable
 @NoArgsConstructor
 @AllArgsConstructor
