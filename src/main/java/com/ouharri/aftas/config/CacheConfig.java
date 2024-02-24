010c4ab29110d6b2d1b787ad68ef46d3844c08a4
ouharri <ouharrioutman@gmail.com>
2024-02-24 16:54:59 +0000

    Configuration class for Redis And app caching.

---
+++ src/main/java/com/ouharri/aftas/config/CacheConfig.java
@@ -0,0 +1,87 @@
+package com.ouharri.aftas.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+/**
+ * Configuration class for Redis And app caching.
+ *
+ * @author <a href="mailto:ouharrioutman@gmail.com">ouharri outman</a>
+ */
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    /**
+     * Bean definition for the LettuceConnectionFactory, which is responsible for connecting to the Redis server.
+     *
+     * @return LettuceConnectionFactory bean
+     */
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost, redisPort);
+
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    /**
+     * Bean definition for the RedisCacheManager, which manages caching configurations.
+     *
+     * @return RedisCacheManager bean
+     */
+    @Bean
+    public RedisCacheManager cacheManager() {
+        // Set the default cache configuration with a TTL of 10 minutes and disable caching of null values
+        RedisCacheConfiguration cacheConfig = myDefaultCacheConfig(Duration.ofMinutes(10)).disableCachingNullValues();
+
+        // Build the RedisCacheManager with additional cache configurations for specific caches
+        return RedisCacheManager.builder(redisConnectionFactory())
+                .cacheDefaults(cacheConfig)
+                .build();
+    }
+
+    /**
+     * Helper method to create a default RedisCacheConfiguration with a specified time-to-live (TTL).
+     *
+     * @param duration Time-to-live duration for cache entries
+     * @return RedisCacheConfiguration with the specified TTL
+     */
+    private RedisCacheConfiguration myDefaultCacheConfig(Duration duration) {
+        return RedisCacheConfiguration
+                .defaultCacheConfig()
+                .entryTtl(duration)
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair
+                                .fromSerializer(new GenericJackson2JsonRedisSerializer())
+                );
+    }
+
+    @Bean
+    public KeyGenerator customKeyGenerator() {
+        return (target, method, params) -> {
+            StringBuilder key = new StringBuilder();
+            key.append(method.getName());
+            for (Object param : params) {
+                key.append(':').append(param.toString());
+            }
+            return key.toString();
+        };
+    }
+}
