4a65da57f617fc58468f7660228564dfd8f43aa6
ouharri <ouharrioutman@gmail.com>
2024-02-24 17:23:47 +0000

    Controller class for handling CRUD operations on Competition entities. Exposes RESTful endpoints for managing Competition entities.

--- src/main/java/com/ouharri/aftas/controllers/CompetitionController.java
+++ src/main/java/com/ouharri/aftas/controllers/CompetitionController.java
@@ -1,7 +1,7 @@
 package com.ouharri.aftas.controllers;
 
 import com.ouharri.aftas.model.dto.requests.CompetitionRequest;
-import com.ouharri.aftas.model.dto.responces.CompetitionResponse;
+import com.ouharri.aftas.model.dto.responses.CompetitionResponse;
 import com.ouharri.aftas.services.spec.CompetitionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,13 +15,14 @@ import java.util.UUID;
  * Controller class for handling CRUD operations on Competition entities.
  * Exposes RESTful endpoints for managing Competition entities.
  *
+ * @author <a href="mailto:ouharrioutman@gmail.com">ouharri outman</a>
  * @see _Controller
  */
 @Slf4j
 @Validated
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v2/competition")
+@RequestMapping("/api/v2/competitions")
 public class CompetitionController extends _Controller<UUID, CompetitionRequest, CompetitionResponse, CompetitionService> {
 
 }
