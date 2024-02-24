80449c166a286a22968f7e1e1e14a7b443b85c08
ouharri <ouharrioutman@gmail.com>
2024-02-24 17:05:35 +0000

    Enum representing different types of chat message content. Each enum value corresponds to a specific type of message content, such as text, image, PDF, etc

---
+++ src/main/java/com/ouharri/aftas/model/enums/MessageType.java
@@ -0,0 +1,16 @@
+package com.ouharri.aftas.model.enums;
+
+/**
+ * Enum representing different types of chat message content.
+ *
+ * <p>Each enum value corresponds to a specific type of message content, such as text, image, PDF, etc.</p>
+ *
+ * @version 1.0
+ */
+public enum MessageType {
+    TEXT,
+    IMAGE,
+    PDF,
+    VIDEO,
+    AUDIO,
+}
