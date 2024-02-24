7736e8fbd1a2d83c28652d17d92ee6510a613e67
ouharri <ouharrioutman@gmail.com>
2024-02-24 17:16:56 +0000

    Service class for handling user logout and token revocation.

--- src/main/java/com/ouharri/aftas/security/LogoutService.java
+++ src/main/java/com/ouharri/aftas/security/LogoutService.java
@@ -1,6 +1,9 @@
 package com.ouharri.aftas.security;
 
+import com.ouharri.aftas.exceptions.NoAuthenticateUser;
 import com.ouharri.aftas.repositories.TokenRepository;
+import com.ouharri.aftas.services.spec.JwtService;
+import com.ouharri.aftas.services.spec.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +11,19 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Service class for handling user logout and token revocation.
+ *
+ * @author <a href="mailto:ouharrioutman@gmail.com">Ouharri Outman</a>
  */
 @Service
 @RequiredArgsConstructor
 public class LogoutService implements LogoutHandler {
 
+    private final UserService userService;
+    private final JwtService jwtService;
     private final TokenRepository tokenRepository;
 
     /**
@@ -25,7 +33,7 @@ public class LogoutService implements LogoutHandler {
      * @param response       HttpServletResponse
      * @param authentication Authentication object representing the current user's authentication details
      */
-    @Override
+    @Transactional
     public void logout(
             HttpServletRequest request,
             HttpServletResponse response,
@@ -34,19 +42,20 @@ public class LogoutService implements LogoutHandler {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
 
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            return;
-        }
+        if (authHeader == null || !authHeader.startsWith("Bearer "))
+            throw new NoAuthenticateUser("Token not found");
 
         jwt = authHeader.substring(7);
         var storedToken = tokenRepository.findByToken(jwt)
-                .orElse(null);
+                .orElseThrow(() -> new NoAuthenticateUser("Token not found"));
 
-        if (storedToken != null) {
+        if (!storedToken.isExpired() && !storedToken.isRevoked()) {
+            userService.disconnect(storedToken.getUser());
             storedToken.setExpired(true);
             storedToken.setRevoked(true);
             tokenRepository.save(storedToken);
             SecurityContextHolder.clearContext();
-        }
+        } else
+            throw new NoAuthenticateUser("No user is authenticated");
     }
 }
