88edc558408dfb83ebb5c09e6dc7b4830f8bdfb5
ouharri <ouharrioutman@gmail.com>
2024-02-24 17:13:36 +0000

    Enum representing various permissions that can be granted to roles in the system. Each permission corresponds to a specific action or set of actions that a user is allowed to perform.

--- src/main/java/com/ouharri/aftas/model/enums/Permission.java
+++ src/main/java/com/ouharri/aftas/model/enums/Permission.java
@@ -3,24 +3,115 @@ package com.ouharri.aftas.model.enums;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Enum representing various permissions that can be granted to roles in the system.
+ * Each permission corresponds to a specific action or set of actions that a user is allowed to perform.
+ *
+ * @author <a href="mailto:ouharrioutman@gmail.com">Ouharri Outman</a>
+ */
 @Getter
 @RequiredArgsConstructor
 public enum Permission {
 
+    /**
+     * Permission to read admin-related data.
+     */
     ADMIN_READ("admin:read"),
+
+    /**
+     * Permission to update admin-related data.
+     */
     ADMIN_UPDATE("admin:update"),
+
+    /**
+     * Permission to create admin-related data.
+     */
     ADMIN_CREATE("admin:create"),
+
+    /**
+     * Permission to delete admin-related data.
+     */
     ADMIN_DELETE("admin:delete"),
 
+    /**
+     * Permission to read management-related data.
+     */
     MANAGER_READ("management:read"),
+
+    /**
+     * Permission to update management-related data.
+     */
     MANAGER_UPDATE("management:update"),
+
+    /**
+     * Permission to create management-related data.
+     */
     MANAGER_CREATE("management:create"),
+
+    /**
+     * Permission to delete management-related data.
+     */
     MANAGER_DELETE("management:delete"),
 
-    SUPER_ADMINISTRATOR_READ("admin:read"),
-    SUPER_ADMINISTRATOR_UPDATE("admin:update"),
-    SUPER_ADMINISTRATOR_CREATE("admin:create"),
-    SUPER_ADMINISTRATOR_DELETE("admin:delete");
+    /**
+     * Permission to read player-related data.
+     */
+    PLAYER_READ("player:read"),
+
+    /**
+     * Permission to update player-related data.
+     */
+    PLAYER_UPDATE("player:update"),
+
+    /**
+     * Permission to create player-related data.
+     */
+    PLAYER_CREATE("player:create"),
+
+    /**
+     * Permission to delete player-related data.
+     */
+    PLAYER_DELETE("player:delete"),
+
+    /**
+     * Permission to read jury-related data.
+     */
+    JURY_READ("jury:read"),
+
+    /**
+     * Permission to update jury-related data.
+     */
+    JURY_UPDATE("jury:update"),
+
+    /**
+     * Permission to create jury-related data.
+     */
+    JURY_CREATE("jury:create"),
+
+    /**
+     * Permission to delete jury-related data.
+     */
+    JURY_DELETE("jury:delete"),
+
+    /**
+     * Permission for super administrator to read data. Generally grants access to more sensitive or comprehensive data.
+     */
+    SUPER_ADMINISTRATOR_READ("super-admin:read"),
+
+    /**
+     * Permission for super administrator to update data.
+     */
+    SUPER_ADMINISTRATOR_UPDATE("super-admin:update"),
+
+    /**
+     * Permission for super-administrator to create data.
+     */
+    SUPER_ADMINISTRATOR_CREATE("super-admin:create"),
+
+    /**
+     * Permission for super administrator to delete data.
+     */
+    SUPER_ADMINISTRATOR_DELETE("super-admin:delete");
 
     private final String permission;
 }
