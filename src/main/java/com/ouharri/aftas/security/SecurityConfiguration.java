0fd4650590922679dd9f7fe0294f1cdb49daf0a0
ouharri <ouharrioutman@gmail.com>
2024-02-24 17:17:34 +0000

    Configuration class for Spring Security settings. This class configures the security aspects of the application, including CORS settings, CSRF protection, session management, authentication providers, and JWT filter integration. It also defines the security filter chain that specifies which requests are secured and how.

--- src/main/java/com/ouharri/aftas/security/SecurityConfiguration.java
+++ src/main/java/com/ouharri/aftas/security/SecurityConfiguration.java
@@ -3,13 +3,14 @@ package com.ouharri.aftas.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -17,21 +18,28 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 /**
  * Configuration class for Spring Security settings.
+ * This class configures the security aspects of the application, including CORS settings,
+ * CSRF protection, session management, authentication providers, and JWT filter integration.
+ * It also defines the security filter chain that specifies which requests are secured and how.
+ *
+ * @author <a href="mailto:ouharrioutman@gmail.com">Ouharri Outman</a>
  */
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 @EnableMethodSecurity
 public class SecurityConfiguration {
-    private static final String[] WHITE_LIST_URL = {
+
+    private static final List<String> WHITE_LIST_URL = List.of(
             "/api/v2/**",
             "/api/v2/auth/**",
+            "/api/v2/Oauth/**",
             "/v2/api-docs",
             "/v3/api-docs",
             "/v3/api-docs/**",
@@ -41,82 +49,125 @@ public class SecurityConfiguration {
             "/configuration/security",
             "/swagger-ui/**",
             "/webjars/**",
-            "/swagger-ui.html"
-    };
-    private static final String[] ALLOW_ORIGIN = {
-            "http://localhost:4200"
-    };
-    private static final String[] ALLOW_METHODS = {
+            "/swagger-ui.html",
+            "/actuator/**"
+    );
+    private static final List<String> ALLOW_ORIGIN = List.of(
+            "http://localhost:4200",
+            "https://e044-197-230-250-154.ngrok-free.app"
+    );
+    private static final List<String> ALLOW_METHODS = List.of(
             "GET",
             "POST",
             "PUT",
             "DELETE",
             "OPTIONS"
-    };
-    private static final String[] ALLOW_HEAD = {
+    );
+    private static final List<String> ALLOW_HEAD = List.of(
             "Access-Control-Allow-Origin",
             "Access-Control-Allow-Methods",
             "Access-Control-Allow-Headers",
             "Access-Control-Max-Age",
             "Access-Control-Request-Headers",
             "Access-Control-Request-Method",
+            "accept",
+            "authorization",
+            "content-type",
+            "user-agent",
+            "x-csrftoken",
+            "x-requested-with",
+            "ngrok-skip-browser-warning",
             "Origin",
             "Cache-Control",
             "Content-Type",
             "Authorization",
+            "Accept",
             "X-Requested-With"
-    };
+    );
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler logoutHandler;
 
     /**
-     * Configures security settings for the application, including authentication, authorization, and JWT handling.
+     * Configures the security filter chain for the application.
+     * This method sets up CORS, CSRF protection, session management, and configures authentication
+     * and authorization for HTTP requests. It also integrates the JWT authentication filter and
+     * defines logout behavior.
      *
-     * @param http HttpSecurity object to configure security settings
-     * @return SecurityFilterChain object representing the configured security filter chain
-     * @throws Exception if an error occurs during the configuration process
+     * @param http HttpSecurity object to configure security settings.
+     * @return SecurityFilterChain object representing the configured security filter chain.
+     * @throws Exception if an error occurs during the configuration process.
      */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        AntPathRequestMatcher[] requestMatchers = Arrays.stream(WHITE_LIST_URL)
-                .map(AntPathRequestMatcher::new)
-                .toArray(AntPathRequestMatcher[]::new);
         http
                 .cors(httpSecurityCorsConfigurer ->
                         httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource())
                 )
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(req ->
-                        req.requestMatchers(requestMatchers)
-                                .permitAll()
-                                .anyRequest()
-                                .authenticated()
+                .exceptionHandling(customizer ->
+                        customizer.authenticationEntryPoint(
+                                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
+                        )
                 )
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(STATELESS)
                 )
+                .headers(headers -> headers
+                        .httpStrictTransportSecurity(hsts -> hsts
+                                .includeSubDomains(true)
+                                .maxAgeInSeconds(31536000)
+                        )
+                )
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(createWhiteListMatchers())
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(
+                        jwtAuthFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                )
                 .logout(logout ->
-                        logout.logoutUrl("/api/v1/auth/logout")
+                        logout.logoutUrl("/api/v2/auth/logout")
                                 .addLogoutHandler(logoutHandler)
                                 .logoutSuccessHandler((request, response, authentication) ->
-                                        SecurityContextHolder.clearContext()
+                                        response.setStatus(HttpStatus.OK.value())
                                 )
                 );
         return http.build();
     }
 
+    /**
+     * Configures CORS (Cross-Origin Resource Sharing) settings for the application.
+     * This method defines which origins, HTTP methods, and headers are allowed in CORS requests.
+     *
+     * @return CorsConfigurationSource object representing the CORS configuration.
+     */
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(ALLOW_ORIGIN));
-        configuration.setAllowedMethods(Arrays.asList(ALLOW_METHODS));
-        configuration.setAllowedHeaders(Arrays.asList(ALLOW_HEAD));
+        configuration.setAllowedOrigins(ALLOW_ORIGIN);
+        configuration.setAllowedMethods(ALLOW_METHODS);
+        configuration.setAllowedHeaders(ALLOW_HEAD);
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
+    /**
+     * Creates URL matchers based on the whitelist.
+     * Used to define paths exempt from authentication.
+     *
+     * @return an array of AntPathRequestMatcher based on the whitelist URLs.
+     */
+    private AntPathRequestMatcher[] createWhiteListMatchers() {
+        return WHITE_LIST_URL.stream()
+                .map(AntPathRequestMatcher::new)
+                .toArray(AntPathRequestMatcher[]::new);
+    }
 }
