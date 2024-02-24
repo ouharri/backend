1394ab5a5aef947c97b59c9f7631061bc63bd7b1
ouharri <ouharrioutman@gmail.com>
2024-02-24 17:47:09 +0000

    Represents a member of the sports club. Extends the AbstractEntity class.

--- src/main/java/com/ouharri/aftas/model/entities/Member.java
+++ src/main/java/com/ouharri/aftas/model/entities/Member.java
@@ -1,19 +1,11 @@
 package com.ouharri.aftas.model.entities;
 
-import com.ouharri.aftas.model.enums.Gender;
 import com.ouharri.aftas.model.enums.IdentityDocumentType;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
-import org.hibernate.validator.constraints.URL;
 
 import java.sql.Date;
 import java.util.ArrayList;
@@ -23,29 +15,18 @@ import java.util.List;
  * Represents a member of the sports club.
  * Extends the AbstractEntity class.
  *
- * @author ouharri
+ * @author <a href="mailto:ouharrioutman@gmail.com">Ouharri Outman</a>
  * @version 1.0
  */
 @Getter
 @Setter
 @Entity
 @SuperBuilder
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "member")
-public class Member extends AbstractEntity {
-
-    /**
-     * The name of the member.
-     */
-    @NotBlank(message = "Name cannot be blank.")
-    private String name;
-
-    /**
-     * The family name of the member.
-     */
-    @NotBlank(message = "Family name cannot be blank.")
-    private String familyName;
+public class Member extends User {
 
     /**
      * The date of accession to the club.
@@ -55,30 +36,9 @@ public class Member extends AbstractEntity {
     private Date accessionDate;
 
     /**
-     * The nationality of the member.
-     */
-    @NotBlank(message = "Nationality cannot be blank.")
-    private String nationality;
-
-    /**
-     * The birthdate of the member.
-     */
-    @Temporal(TemporalType.DATE)
-    @NotNull(message = "Birth date cannot be null.")
-    private Date birthDate;
-
-    /**
-     * The email of the member.
-     */
-    @Email(message = "Email was not provided")
-    @NotNull(message = "Email cannot be null.")
-    private String email;
-
-    /**
      * The type of identity document.
      */
     @Enumerated(EnumType.STRING)
-    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
     @NotNull(message = "Identity document type cannot be null.")
     private IdentityDocumentType identityDocument;
 
@@ -87,19 +47,6 @@ public class Member extends AbstractEntity {
      */
     @NotBlank(message = "Identity number cannot be blank.")
     private String identityNumber;
-
-    /**
-     * The member's gender.
-     */
-    @Enumerated(EnumType.STRING)
-    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
-    private Gender gender;
-
-    /**
-     * The member's image.
-     */
-    @URL
-    private String image;
 
     /**
      * The list of huntings associated with this member.
