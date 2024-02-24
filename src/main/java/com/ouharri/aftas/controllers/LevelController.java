75f513d78fb74fdf2743d0b656a17da61b676bf7
ouharri <ouharrioutman@gmail.com>
2024-02-24 17:24:25 +0000

    Controller class for handling CRUD operations on Level entities. Exposes RESTful endpoints for managing Level entities, including creation, retrieval, update, and deletion.

--- src/main/java/com/ouharri/aftas/controllers/LevelController.java
+++ src/main/java/com/ouharri/aftas/controllers/LevelController.java
@@ -1,7 +1,7 @@
 package com.ouharri.aftas.controllers;
 
 import com.ouharri.aftas.model.dto.requests.LevelRequest;
-import com.ouharri.aftas.model.dto.responces.LevelResponse;
+import com.ouharri.aftas.model.dto.responses.LevelResponse;
 import com.ouharri.aftas.services.spec.LevelService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,13 +15,14 @@ import java.util.UUID;
  * Controller class for handling CRUD operations on Level entities.
  * Exposes RESTful endpoints for managing Level entities, including creation, retrieval, update, and deletion.
  *
+ * @author <a href="mailto:ouharrioutman@gmail.com">ouharri outman</a>
  * @see _Controller
  */
 @Slf4j
 @Validated
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v2/level")
+@RequestMapping("/api/v2/levels")
 public class LevelController extends _Controller<UUID, LevelRequest, LevelResponse, LevelService> {
 
 }
