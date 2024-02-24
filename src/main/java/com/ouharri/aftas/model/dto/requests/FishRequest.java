457dd7d495a63b21f4f3142f5b126637853d1313
ouharri <ouharrioutman@gmail.com>
2024-02-24 17:40:33 +0000

    DTO for Fish

--- src/main/java/com/ouharri/aftas/model/dto/requests/FishRequest.java
+++ src/main/java/com/ouharri/aftas/model/dto/requests/FishRequest.java
@@ -1,11 +1,12 @@
 package com.ouharri.aftas.model.dto.requests;
 
-import com.ouharri.aftas.model.dto.responces.LevelResponse;
+import com.ouharri.aftas.model.dto.responses.LevelResponse;
+import com.ouharri.aftas.model.entities.Fish;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 /**
- * DTO for {@link com.ouharri.aftas.model.entities.Fish}
+ * DTO for {@link Fish}
  */
 public record FishRequest(
         @NotBlank(message = "The name cannot be blank.")
